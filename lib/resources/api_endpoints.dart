9877bcc2e00f68588671781c21964eba5b3a1569
Mohammad2464 <nisarqayyum911@gmail.com>
2025-04-25 00:10:06 -0700

    second commit

--- lib/resources/api_endpoints.dart
+++ lib/resources/api_endpoints.dart
@@ -1,7 +1,7 @@
 class ApiEndPoints {
 
   static const String baseUrl = 'https://dummyjson.com/products';
-  static const String products = '$baseUrl/?limit=10';
+  static const String products = '$baseUrl/?limit=100';
     static const String categories = '$baseUrl/categories';
         static const String fetchProductsByCategory = '$baseUrl/category';
 
